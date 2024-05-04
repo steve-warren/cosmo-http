89255f2d7d0eaec685db984606895d4e098cb381
Steve Warren <steve-warren@users.noreply.github.com>
2024-05-03 21:41:31 -0500

    add dynamic request support

---
+++ HttpServer.cs
@@ -0,0 +1,196 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Channels;
+
+namespace Cosmo.Http;
+
+public record HttpRequest(string Verb, string Uri, string Version);
+
+public class HttpResponse
+{
+    public string ContentType { get; set; } = "";
+    public string Content { get; set; } = "";
+}
+
+public sealed class HttpServer
+{
+    private readonly CancellationTokenSource _cts;
+    private readonly CancellationToken _hostCancellationToken;
+    private readonly Channel<Task> _pendingRequests;
+    private readonly Dictionary<string, Action<HttpRequest, HttpResponse>> _routes;
+
+    public HttpServer(
+        string endpoint,
+        int port,
+        Dictionary<string, Action<HttpRequest, HttpResponse>> routes
+    )
+    {
+        _cts = new();
+        _hostCancellationToken = _cts.Token;
+        _routes = routes;
+        _pendingRequests = Channel.CreateUnbounded<Task>();
+
+        Endpoint = endpoint;
+        Port = port;
+    }
+
+    public string Endpoint { get; private set; }
+    public int Port { get; private set; }
+
+    public async Task RunAsync()
+    {
+        Console.WriteLine("Starting server...");
+
+        using var serverSocket = Bind(Endpoint, Port);
+
+        Console.CancelKeyPress += (sender, e) =>
+        {
+            e.Cancel = true;
+            SignalShutdown();
+        };
+
+        AppDomain.CurrentDomain.ProcessExit += (sender, e) =>
+        {
+            SignalShutdown();
+        };
+
+        var producer = Producer(serverSocket, _pendingRequests.Writer, _cts.Token);
+        var consumer = Consumer(_pendingRequests.Reader, _cts.Token);
+
+        await Task.WhenAll([producer, consumer]);
+
+        Console.WriteLine("Server stopped.");
+    }
+
+    public void Shutdown() => SignalShutdown();
+
+    private async Task Producer(
+        Socket serverSocket,
+        ChannelWriter<Task> writer,
+        CancellationToken cancellationToken
+    )
+    {
+        while (true)
+        {
+            try
+            {
+                var clientSocket = await serverSocket.AcceptAsync(cancellationToken);
+
+                var receiveTask = ReceiveAsync(clientSocket, cancellationToken);
+
+                writer.TryWrite(receiveTask);
+            }
+            catch (OperationCanceledException)
+            {
+                writer.Complete();
+                break;
+            }
+            catch (Exception ex)
+            {
+                writer.Complete(ex);
+                throw;
+            }
+        }
+    }
+
+    private async Task Consumer(ChannelReader<Task> reader, CancellationToken cancellationToken)
+    {
+        try
+        {
+            while (await reader.WaitToReadAsync(cancellationToken))
+            {
+                if (reader.TryRead(out var receiveTask))
+                {
+                    if (!receiveTask.IsCompleted)
+                        await receiveTask;
+                }
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            return;
+        }
+    }
+
+    private static Socket Bind(string endpoint, int port)
+    {
+        var localhost = IPAddress.Parse(endpoint);
+        var ipEndpoint = new IPEndPoint(localhost, port);
+
+        var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+        socket.Bind(ipEndpoint);
+        socket.Listen();
+
+        Console.WriteLine($"Listening on http://{ipEndpoint.Address}:{ipEndpoint.Port}");
+
+        return socket;
+    }
+
+    private async Task ReceiveAsync(Socket clientSocket, CancellationToken cancellationToken)
+    {
+        using (clientSocket)
+        {
+            var buffer = new byte[1024];
+
+            var received = await clientSocket.ReceiveAsync(
+                buffer,
+                SocketFlags.None,
+                cancellationToken
+            );
+
+            var httpRequest = ParseRequest(new ArraySegment<byte>(buffer, 0, received));
+            Console.WriteLine($"route {httpRequest.Uri}");
+
+            if (_routes.TryGetValue(httpRequest.Uri, out var responseHandler))
+            {
+                var httpResponse = new HttpResponse();
+
+                responseHandler(httpRequest, httpResponse);
+
+                var rawResponseBody = Encoding.UTF8.GetBytes(httpResponse.Content);
+                var response = Encoding.UTF8.GetBytes(
+                    $"HTTP/1.1 200 OK\r\nContent-Type:{httpResponse.ContentType}\r\nContent-Length: {rawResponseBody.Length}\r\n\r\n"
+                );
+
+                await clientSocket.SendAsync(response, cancellationToken);
+                await clientSocket.SendAsync(rawResponseBody, cancellationToken);
+            }
+            else
+            {
+                await clientSocket.SendAsync(
+                    Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n"),
+                    cancellationToken
+                );
+            }
+        }
+    }
+
+    private void SignalShutdown()
+    {
+        if (!_cts.IsCancellationRequested)
+        {
+            Console.WriteLine("\nShutting down...");
+            _cts.Cancel();
+        }
+    }
+
+    private static HttpRequest ParseRequest(ArraySegment<byte> requestLineSegment)
+    {
+        var requestBody = Encoding.UTF8.GetString(requestLineSegment).AsSpan();
+
+        //GET / HTTP/1.1\r\n
+        const string crlf = "\r\n";
+
+        var requestLine = requestBody[..requestBody.IndexOf(crlf)];
+        var httpVerb = requestLine[..requestLine.IndexOf(' ')];
+
+        var uri = requestLine.Slice(
+            requestLine.IndexOf(' ') + 1,
+            requestLine.LastIndexOf(' ') - requestLine.IndexOf(' ') - 1
+        );
+
+        // todo - avoid string alloc
+        return new HttpRequest(new string(httpVerb), new string(uri), "HTTP/1.1");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,127 +1,37 @@
-﻿using System.Net;
-using System.Net.Sockets;
-using System.Text;
+﻿using Cosmo.Http;
 
 using var cts = new CancellationTokenSource();
 var hostCancellationToken = cts.Token;
 
-var routes = MapRoutes();
-
-var serverSocket = StartServer(endpoint: "127.0.0.1", port: 8080);
-
-while (true)
+var routes = new Dictionary<string, Action<HttpRequest, HttpResponse>>
 {
-    try
     {
-        var clientSocket = await serverSocket.AcceptAsync(hostCancellationToken);
-
-        ReceiveAsync(clientSocket, hostCancellationToken);
-    }
-    catch (OperationCanceledException)
-    {
-        break;
-    }
-}
-
-Console.WriteLine("Server stopped.");
-
-async void ReceiveAsync(Socket clientSocket, CancellationToken cancellationToken)
-{
-    using (clientSocket)
-    {
-        var buffer = new byte[1024];
-
-        var received = await clientSocket.ReceiveAsync(buffer, SocketFlags.None, cancellationToken);
-
-        var httpRequest = ParseRequest(new ArraySegment<byte>(buffer, 0, received));
-
-        if (routes.TryGetValue(httpRequest.Uri, out var responseHandler))
+        "/",
+        (request, response) =>
         {
-            Console.WriteLine($"route {httpRequest.Uri}");
-
-            var responseBody = Encoding.UTF8.GetBytes(responseHandler());
-
-            var response =
-                $"HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=UTF-8\r\nContent-Length: {responseBody.Length}\r\n\r\n";
-
-            await clientSocket.SendAsync(Encoding.UTF8.GetBytes(response));
-            await clientSocket.SendAsync(responseBody);
+            response.Content = "hello, world!";
+            response.ContentType = "text/plain";
         }
-        else
-            await clientSocket.SendAsync(
-                Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n"),
-                cancellationToken
-            );
-
-        clientSocket.Shutdown(SocketShutdown.Both);
-        clientSocket.Close();
+    },
+    {
+        "/status",
+        (request, response) =>
+        {
+            response.Content = """{"here" : ["is", "some", "json"]}""";
+            response.ContentType = "application/json";
+        }
+    },
+    {
+        "/index.html",
+        (request, response) =>
+        {
+            response.Content =
+                "<html><head><title>index.html</title><body><h1>hello, world!</body></html>";
+            response.ContentType = "text/html";
+        }
     }
-}
+};
 
-Socket StartServer(string endpoint, int port)
-{
-    var localhost = IPAddress.Parse(endpoint);
-    var ipEndpoint = new IPEndPoint(localhost, port);
+var server = new HttpServer(endpoint: "127.0.0.1", 8080, routes);
 
-    Console.CancelKeyPress += (sender, e) =>
-    {
-        e.Cancel = true;
-
-        ShutdownServer();
-    };
-
-    AppDomain.CurrentDomain.ProcessExit += (sender, e) =>
-    {
-        ShutdownServer();
-    };
-
-    var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
-    socket.Bind(ipEndpoint);
-    socket.Listen();
-
-    Console.WriteLine($"Listening on http://{ipEndpoint.Address}:{ipEndpoint.Port}");
-
-    return socket;
-}
-
-void ShutdownServer()
-{
-    if (!cts.IsCancellationRequested)
-    {
-        Console.WriteLine("\nShutting down...");
-        cts.Cancel();
-    }
-}
-
-Dictionary<string, Func<string>> MapRoutes()
-{
-    var routes = new Dictionary<string, Func<string>>
-    {
-        { "/", () => "home" },
-        { "/about", () => "about" },
-        { "/status", () => DateTimeOffset.Now.ToString() }
-    };
-
-    return routes;
-}
-
-HttpRequest ParseRequest(ArraySegment<byte> requestLineSegment)
-{
-    var requestBody = Encoding.UTF8.GetString(requestLineSegment).AsSpan();
-
-    //GET / HTTP/1.1\r\n
-    const string crlf = "\r\n";
-
-    var requestLine = requestBody[..requestBody.IndexOf(crlf)];
-    var httpVerb = requestLine[..requestLine.IndexOf(' ')];
-
-    var uri = requestLine.Slice(
-        requestLine.IndexOf(' ') + 1,
-        requestLine.LastIndexOf(' ') - requestLine.IndexOf(' ') - 1
-    );
-
-    // todo - avoid string alloc
-    return new HttpRequest(new string(httpVerb), new string(uri), "HTTP/1.1");
-}
-
-record HttpRequest(string Verb, string Uri, string Version);
+await server.RunAsync();
